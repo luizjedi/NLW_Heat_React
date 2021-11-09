18dd102083a479571ed9abff92beb0573320fb90
Luiz Felipe de Oliveira <luiz.pepe@hotmail.com>
2021-11-09 15:45:19 -0300

    Término do front web

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { api } from '../../services/api';
+import { io } from 'socket.io-client';
 
 import logoImg from '../../assets/logo.svg';
 import styles from './styles.module.scss';
@@ -13,11 +14,33 @@ type Message = {
     }
 }
 
+const messagesQueue: Message[] = [];
+
+const socket = io('http://localhost:4000');
+
+socket.on('new_message', (newMessage: Message) => {
+    messagesQueue.push(newMessage);
+})
+
 export function MessageList() {
     const [messages, setMessages] = useState<Message[]>([])
 
     useEffect(() => {
-        api.get<Message[]>('messages/last3').then(response => {
+        const timer = setInterval(() => {
+            if (messagesQueue.length > 0) {
+                setMessages(prevState => [
+                    messagesQueue[0],
+                    prevState[0],
+                    prevState[1],
+                ].filter(Boolean))
+
+                messagesQueue.shift();
+            }
+        }, 1000)
+    }, [])
+
+    useEffect(() => {
+        api.get<Message[]>('messages/last5').then(response => {
             setMessages(response.data)
         })
     }, [])
